bdf99e9b579662071f104401b12a6b8bc7aa1fe7
Jannis R <mail@jannisr.de>
2017-01-24 20:28:26 +0100

    implementation :clap:

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
 'use strict'
 
-// todo
+if (process.env.NODE_ENV === 'dev') {
+	const immutable = require('seamless-immutable')
+
+	const wrap = (component) => {
+		if (component.length !== 2) {
+			const name = component.name || 'Your component'
+			throw new Error(name + ' must take exactly 2 arguments.')
+		}
+
+		const dataDownActionsUp = (data, actions) => {
+			data = immutable(data)
+
+			if ('object' !== typeof actions)
+				throw new Error('actions must be an object')
+			for (let name in actions) {
+				// todo: nested objects
+				if('function' !== typeof actions[name])
+					throw new Error('actions object must only contain functions')
+			}
+
+			return component(data, actions)
+		}
+
+		return dataDownActionsUp
+	}
+
+	module.exports = wrap
+} else {
+	module.exports = (component) => component
+}
